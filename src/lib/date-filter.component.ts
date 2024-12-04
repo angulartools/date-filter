20e49ce57702adafd7bcea73761925c7e1dc09e1
danieleespindola-brenntag <daniele.espindola@brenntag-external.com>
2024-12-04 11:08:53 -0300

    First Commit

---
+++ src/lib/date-filter.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-date-filter',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      date-filter works!
+    </p>
+  `,
+  styles: ``
+})
+export class DateFilterComponent {
+
+}
---
+++ src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of date-filter
+ */
+
+export * from './lib/date-filter.service';
+export * from './lib/date-filter.component';
